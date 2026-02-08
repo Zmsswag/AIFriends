a4e9f04b3867f034da8ef8237cc5a5dac643b184
zms <3090428958@xiaomi.com>
2026-02-08 16:10:13 +0800

    实现增删改查

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from web.models.user import UserProfile
+from web.models.character import Character
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)   #逗号不可删，代表变成列表，而不是单一元素
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author',)
